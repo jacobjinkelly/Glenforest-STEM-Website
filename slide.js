f871cfb3be86d2823d84344f9a798ffde1d39b72
jacobjinkelly <jacob.jin.kelly@gmail.com>
2016-12-03 00:23:27 -0500

    added new events and images
    globe and mail, gtaa, itac

--- slide.js
+++ slide.js
@@ -1,6 +1,6 @@
-var posts=["nahani","mayor","stemposium","cpa","cisco","cityCouncil","olympics","outreach","healthTalk"];
-var indices=[0,0,0,0,0,0,0,0,0];
-var nposts=9;
+var posts=["nahani","itac","gtaa","mayor","stemposium","cpa","cisco","cityCouncil","olympics","outreach","healthTalk"];
+var indices=[0,0,0,0,0,0,0,0,0,0,0];
+var nposts=11;
 
 $(document).ready(function() {
   for (var i=0;i<nposts;i++) showDivs(posts[i],indices[i]);
